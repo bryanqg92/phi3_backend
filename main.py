b97d1abb28043b44ad2951de51ab00177992d59f
bryanqg92@gmail.com <bryanqg92@gmail.com>
2024-07-07 00:55:13 -0500

    addingg main to test

---
+++ main.py
@@ -0,0 +1,27 @@
+from src.models.phi3 import phi3_model
+from src.text_splitter.DocTextSplitter import DocTextSplitter
+from src.chains.QARetrievalChain import QARetrievalChain
+from src.chains.QARetrievalReranker import QARetrievalReranker
+
+def main(prompt:str):
+
+    print("\n   ==> Starting Application")
+    print(f"\n   ==> {prompt}")
+    model = phi3_model()
+    print("\n   ==> Loaded Model")
+
+    DocTextSplitter.LoadAndSplit("docs/Informe Final CD.pdf")
+    print("\n   ==>Loaded Documents and splitted")
+
+    #qa = QARetrievalChain(model.getPipeline()).GetQAChain()
+    qa = QARetrievalReranker(model.getPipeline()).GetQAChain()
+    print("\n   ==>Loaded QA Chain")
+
+    query = {"query": prompt}
+    response = qa.invoke(query)
+    print("\n   Response: " + response['result'].split("###")[-1].strip())
+
+
+
+if __name__ == "__main__":
+    main("Hay conjuntos difusos?")
